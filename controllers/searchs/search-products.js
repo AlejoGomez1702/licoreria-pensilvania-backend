89186c5589e2918728ad3b52b613ac4d4d0aa1ae
LUIS ALEJANDRO GOMEZ CASTANO <LAGOMCAS@davivienda.loc>
2022-06-04 15:39:29 -0500

    Listo el buscador en el inventario

--- controllers/searchs/search-products.js
+++ controllers/searchs/search-products.js
@@ -12,7 +12,7 @@ const { Product } = require("../../models");
  const searchProducts = async( term = '', req = request, res = response ) => {
 
     const supercategory = req.supercategory;
-    const { limit = 8, from = 0 } = req.query;
+    const { limit = 8, from = 0, category = '' } = req.query;
 
     // Buscar por ID
     const isMongoID = isValidObjectId( term );
@@ -25,67 +25,107 @@ const { Product } = require("../../models");
         });
     }
 
-    const establishment = req.establishmentId;
+    const establishment = req.user.establishment;
     let basicQuery = [{ state: true }, { establishment }];
     
     if( supercategory )
     {
-        basicQuery = [{ state: true }, { establishment }, { supercategory }];
+        basicQuery.push({ supercategory });
+        // basicQuery = [{ state: true }, { establishment }, { supercategory }];
     }
+
+    if( category )
+    {
+        basicQuery.push({ category });
+    }
+
+    // Buscar por coincidencia
+    const regex = new RegExp( term, 'i' );
+    const productQuery = {
+        $or: [
+            { name: regex }, 
+            { description: regex }
+        ],
+        $and: basicQuery,                                
+    };
+    const [ total, products ] = await Promise.all([
+        Product.countDocuments(productQuery),
+        Product.find(productQuery)
+                        .populate('establishment', 'name')
+                        .populate('category', 'name')
+                        .populate('unit', 'unit units')
+                        .skip( Number( from ) )  // desde donde
+                        .limit( Number( limit ) ) // Cuantos
+    ]);
+
+
+    // Product.find({
+    //     $or: [
+    //         { name: regexProductName }, 
+    //         { description: regexProductName }
+    //     ],
+    //     $and: basicQuery,                                
+    // })
+    // .populate('category', 'name')
+    // .populate('unit', 'unit'),
+
+
+
+
     
     // Buscar por coincidencia
     console.log("Termino::: ", term);
-    let termSplited = term.split(' ');
-    const termProductCategory = termSplited.shift();
-    const regexProductCategory = new RegExp( termProductCategory, 'i' );
-    const termProductName = termSplited.toString().replace( ',',' ' );
-    const regexProductName = new RegExp( termProductName, 'i' );
+    // let termSplited = term.split(' ');
+    // const termProductCategory = termSplited.shift();
+    // const regexProductCategory = new RegExp( termProductCategory, 'i' );
+    // const termProductName = termSplited.toString().replace( ',',' ' );
+    // const regexProductName = new RegExp( termProductName, 'i' );
 
-    const regex = new RegExp( term, 'i' );
-    const querys = {
-        category : { path: 'category', match: {name: regexProductCategory}, select: 'name' },
-        category2 : { path: 'category', match: {name: regexProductName}, select: 'name' },
-        unit : { path: 'unit', match: {unit: regex}, select: 'unit' } 
-    };
+    // const regex = new RegExp( term, 'i' );
+    // const querys = {
+    //     category : { path: 'category', match: {name: regexProductCategory}, select: 'name' },
+    //     category2 : { path: 'category', match: {name: regexProductName}, select: 'name' },
+    //     unit : { path: 'unit', match: {unit: regex}, select: 'unit' } 
+    // };
 
-    let productsTotal = [];
+    // let productsTotal = [];
 
-    let [ 
-        productsMatch, 
-        productsMatch2, 
-        productsMatchCategory, 
-        productsMatchCategory2
-    ] = await Promise.all([
-        // ProductsMatch
-        Product.find({
-                    $or: [
-                        { name: regexProductName }, 
-                        { description: regexProductName }
-                    ],
-                    $and: basicQuery,                                
-                })
-                .populate('category', 'name')
-                .populate('unit', 'unit'),
-        // productsMatch2
-        Product.find({
-                    $or: [
-                        { name: regexProductCategory }, 
-                        { description: regexProductCategory }
-                    ],
-                    $and: basicQuery,                                
-                })
-                .populate('category', 'name')
-                .populate('unit', 'unit'),
-        // productsMatchCategory
-        Product.find( { $and: basicQuery } )
-                .populate( querys.category )
-                .populate('unit', 'unit'),
-        // productsMatchCategory2
-        Product.find( { $and: basicQuery } )
-                .populate( querys.category2 )
-                .populate('unit', 'unit')
+    // let [ 
+    //     productsMatch, 
+    //     productsMatch2, 
+    //     productsMatchCategory, 
+    //     productsMatchCategory2
+    // ] = await Promise.all([
+    //     // ProductsMatch
+    //     Product.find({
+    //                 $or: [
+    //                     { name: regexProductName }, 
+    //                     { description: regexProductName }
+    //                 ],
+    //                 $and: basicQuery,                                
+    //             })
+    //             .populate('category', 'name')
+    //             .populate('unit', 'unit'),
+    //     // productsMatch2
+    //     Product.find({
+    //                 $or: [
+    //                     { name: regexProductCategory }, 
+    //                     { description: regexProductCategory }
+    //                 ],
+    //                 $and: basicQuery,                                
+    //             })
+    //             .populate('category', 'name')
+    //             .populate('unit', 'unit'),
+    //     // productsMatchCategory
+    //     Product.find( { $and: basicQuery } )
+    //             .populate( querys.category )
+    //             .populate('unit', 'unit'),
+    //     // productsMatchCategory2
+    //     Product.find( { $and: basicQuery } )
+    //             .populate( querys.category2 )
+    //             .populate('unit', 'unit')
 
-    ]);
+    // ]);
     
     // const productsMatch = await Product.find({
     //                             $or: [
@@ -111,8 +151,8 @@ const { Product } = require("../../models");
     // Los que no coinciden se borran.
     // productsMatch = productsMatch.filter( product => (product.category !== null) );
     // productsMatch2 = productsMatch2.filter( product => (product.category !== null) );
-    productsMatchCategory = productsMatchCategory.filter( product => (product.category !== null) );
-    productsMatchCategory2 = productsMatchCategory2.filter( product => (product.category !== null) );
+    // productsMatchCategory = productsMatchCategory.filter( product => (product.category !== null) );
+    // productsMatchCategory2 = productsMatchCategory2.filter( product => (product.category !== null) );
 
     
 
@@ -137,21 +177,21 @@ const { Product } = require("../../models");
     // // Los que no coinciden se borran.               
     // productsMatchUnit = productsMatchUnit.filter( product => (product.unit !== null) );    
                 
-    productsTotal.push( 
-        ...productsMatch, 
-        ...productsMatch2, 
-        ...productsMatchCategory, 
-        ...productsMatchCategory2
-    );
+    // productsTotal.push( 
+    //     ...productsMatch, 
+    //     ...productsMatch2, 
+    //     ...productsMatchCategory, 
+    //     ...productsMatchCategory2
+    // );
 
-    const products = [ ...new Set(productsTotal) ];
+    // const products = [ ...new Set(productsTotal) ];
 
-    // Hay que sacar del arreglo de productos desde "from" hasta "from + limit"
-    const productsPaginate = products.slice( from, (from + limit) );
+    // // Hay que sacar del arreglo de productos desde "from" hasta "from + limit"
+    // const productsPaginate = products.slice( from, (from + limit) );
 
     return res.json({
         results: products,
-        total: products.length
+        total
     });
 };
 
